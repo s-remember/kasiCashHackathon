fe8345e6f1ea9732abbdfefde0f17f0227f6f261
Kate <credencekate@gmail.com>
2026-09-05 11:40:37 +0200

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using KasiCash.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace KasiCash.Data
+{
+    public class ApplicationDbContext
+        : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<CreditSale> CreditSales { get; set; }
+    }
+}
