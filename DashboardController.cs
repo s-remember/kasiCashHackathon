2f9d1aed37ffa311b28f65c8664c3ed86ed08ada
Kate <credencekate@gmail.com>
2026-09-05 17:21:58 +0200

    Add files via upload

---
+++ DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KasiCash.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
